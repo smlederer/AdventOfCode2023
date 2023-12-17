463b134061cf79cd0b97d13b774135b83782fb08
smlederer <34457857+smlederer@users.noreply.github.com>
2023-12-17 16:52:46 -0500

    lits of stuff 11.2,12.1,13.1,8.2

--- helper_funcs.py
+++ helper_funcs.py
@@ -7,3 +7,13 @@ def read_file(path):
 def modify_raw(string:str):
     mod_input = string.strip().split('\n')
     return mod_input
+
+
+def transpose(matrix:list) -> list:
+    new_matrix = {}
+    for i in range(len(matrix[0])):
+        for j in range(len(matrix)):
+            new_matrix.setdefault(i,[])
+            new_matrix[i].append(matrix[j][i])
+
+    return list(new_matrix.values())
